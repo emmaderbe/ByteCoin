f71069a42d912d5d5f7f0d1ce3438d139b663c0a
emmaderbe <emma-derbe@mail.ru>
2023-02-26 17:35:32 +0300

    first commit

---
+++ ByteCoin/CoinData.swift
@@ -0,0 +1,8 @@
+//
+//  coinData.swift
+//  ByteCoin
+//
+//  Created by Дербе Эмма on 25.02.2023.
+//
+
+import Foundation
---
+++ ByteCoin/CoinManager.swift
@@ -0,0 +1,8 @@
+//
+//  coinЬ.swift
+//  ByteCoin
+//
+//  Created by Дербе Эмма on 25.02.2023.
+//
+
+import Foundation
